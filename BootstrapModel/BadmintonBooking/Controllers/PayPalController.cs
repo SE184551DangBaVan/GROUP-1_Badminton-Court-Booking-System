77b601f890350d6ea29cc5e6a2c1db5d5cdb1735
ThienNH <thiennhse184989@fpt.edu.vn>
2024-06-17 14:01:35 +0700

    Add PayPal Controller and Session package

---
+++ BootstrapModel/BadmintonBooking/Controllers/PayPalController.cs
@@ -0,0 +1,160 @@
+﻿using BadmintonBooking.Repository.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PayPal.Api;
+
+namespace BadmintonBooking.Controllers
+{
+    public class PayPalController : Controller
+    {
+        private IHttpContextAccessor httpContextAccessor;
+        IConfiguration _configuration;
+        private Payment payment;
+
+        public PayPalController(IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+            _configuration = configuration;
+        }
+
+        public ActionResult PaymentWithPaypal(string Cancel = null, string blogId = "", string PayerID = "", string guid = "")
+        {
+            var ClientID = _configuration.GetValue<string>("PayPal:Key");
+            var ClientSecret = _configuration.GetValue<string>("PayPal:Secret");
+            var mode = _configuration.GetValue<string>("PayPal:mode");
+
+            APIContext apiContext = PaypalConfiguration.GetAPIContext(ClientID, ClientSecret, mode);
+
+            try
+            {
+                string payerId = PayerID;
+                if (string.IsNullOrEmpty(payerId))
+                {
+                    string baseURI = this.Request.Scheme + "://" + this.Request.Host + "/PayPal/PaymentWithPaypal?";
+                    string guidd = Convert.ToString(new Random().Next(10000));
+                    guid = guidd;
+
+                    var createdPayment = this.CreatePayment(apiContext, baseURI + "guid=" + guid, blogId);
+
+                    var links = createdPayment.links.GetEnumerator();
+                    string paypalRedirectUrl = null;
+                    while (links.MoveNext())
+                    {
+                        Links Ink = links.Current;
+                        if (Ink.rel.ToLower().Trim().Equals("approval_url"))
+                        {
+                            //saving the payapalredirect URL to which user will be redirected for payment
+                            paypalRedirectUrl = Ink.href;
+                        }
+                    }
+                    // saving the paymentID in the key guid
+                    httpContextAccessor.HttpContext.Session.SetString("payment", createdPayment.id);
+                    return Redirect(paypalRedirectUrl);
+                }
+                else
+                {
+                    var paymentId = httpContextAccessor.HttpContext.Session.GetString("payment");
+                    var executedPayment = ExecutePayment(apiContext, payerId, paymentId as string);
+                    //If executed payment failed then we will show payment failure message to user
+                    if (executedPayment.state.ToLower() != "approved")
+                    {
+                        return View("PaymentFailed");
+                    }
+                    var blogIds = executedPayment.transactions[0].item_list.items[0].sku;
+                    return View("PaymentSuccess");
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("PaymentFailed");
+            }
+            return View("SuccessView");
+        }
+
+        private PayPal.Api.Payment ExecutePayment(APIContext apiContext, string payerId, string paymentId)
+        {
+            var paymentExecution = new PaymentExecution()
+            {
+                payer_id = payerId
+            };
+            this.payment = new Payment()
+            {
+                id = paymentId
+            };
+            return this.payment.Execute(apiContext, paymentExecution);
+        }
+
+
+        private Payment CreatePayment(APIContext apiContext, string redirectUrl, string blogId)
+        {
+            //create itemlist and add item objects to it
+            var itemList = new ItemList()
+            {
+                items = new List<Item>()
+            };
+            {
+                itemList.items.Add(new Item()
+                {
+                    name = "Book Time Slot Fixed",
+                    currency = "USD",
+                    price = "10.00",
+                    quantity = "1",
+                    sku = "BOOK-TS-101"
+                });
+                itemList.items.Add(new Item()
+                {
+                    name = "Book Time Slot Flexible",
+                    currency = "USD",
+                    price = "8.00",
+                    quantity = "1",
+                    sku = "BOOK-TS-102"
+                });
+                var payer = new Payer()
+                {
+                    payment_method = "paypal"
+                };
+
+                var redirUrls = new RedirectUrls()
+                {
+                    cancel_url = redirectUrl + "&Cancel=true",
+                    return_url = redirectUrl
+                };
+
+                var details = new Details()
+                {
+                    tax = "1",
+                    shipping = "1",
+                    subtotal = "18",
+                };
+
+                var amount = new Amount()
+                {
+                    currency = "USD",
+                    total = "20.00", // Total must be equal to sum of tax, shipping and subto //
+                    details = details
+                };
+
+                var transactionList = new List<Transaction>();
+                // Adding description about the transaction
+
+                transactionList.Add(new Transaction()
+                {
+                    description = "Transaction description",
+                    invoice_number = Guid.NewGuid().ToString(), //Generate an Invoice No
+                    amount = amount,
+                    item_list = itemList
+
+                });
+
+                this.payment = new Payment()
+                {
+                    intent = "sale",
+                    payer = payer,
+                    transactions = transactionList,
+                    redirect_urls = redirUrls
+                };
+                return this.payment.Create(apiContext);
+            }
+        }
+    }
+}
--- BootstrapModel/BadmintonBooking/obj/Debug/net8.0/BadmintonBooking.AssemblyInfo.cs
+++ BootstrapModel/BadmintonBooking/obj/Debug/net8.0/BadmintonBooking.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913f3d6107940ba3af0ed460c855bf00826ae5de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce08578ccd4826d13faacec63ff567c4c51ddd4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
