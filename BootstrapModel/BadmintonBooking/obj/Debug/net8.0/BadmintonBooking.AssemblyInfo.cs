ca9103615e5f781a0ff1be91987a28e141de5f79
SE184551DangBaVan <dangbavan2004@gmail.com>
2024-06-21 11:25:32 +0200

    fixes

--- BootstrapModel/BadmintonBooking/obj/Debug/net8.0/BadmintonBooking.AssemblyInfo.cs
+++ BootstrapModel/BadmintonBooking/obj/Debug/net8.0/BadmintonBooking.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7fbd2b897f763ad072a9d641c60da983835fb91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ef528d510523a7cfa56a6544ab128e38181494")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
